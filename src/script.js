73ae800c80b1efcba19e0fa2fb6534affa3ea350
ctoloczko <celina.toloczko@gmail.com>
2024-04-27 20:30:45 +0200

    added select js functionality

--- src/script.js
+++ src/script.js
@@ -22,5 +22,33 @@ function updateTime(event) {
   newyorkTimeElement.innerHTML = newyorkTime;
 }
 
-updateTime();
-setInterval(updateTime, 1000);
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName;
+  if (cityTimeZone.indexOf("/") === -1) {
+    cityName = cityTimeZone;
+  } else {
+    cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  }
+
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+      <div class="city-container">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date" id="stockholm-date">
+            ${cityTime.format("dddd, MMMM Do YYYY")}
+          </div>
+        </div>
+        <div class="time" id="stockholm-time">
+          ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
+        </div>
+      </div>
+    `;
+}
+
+let citySelect = document.querySelector("#city-select");
+citySelect.addEventListener("change", updateCity);
+
+setInterval(updateCity, 1000);
